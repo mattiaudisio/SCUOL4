36d3abc44a115b7aea558eef291d391c7ae38b47
mattiaudisio <mattia.audisio.01@gmail.com>
2020-02-21 11:19:22 +0100

    Aggiunta2

--- SQL/convention_QL.sql
+++ SQL/convention_QL.sql
@@ -1,11 +1,13 @@
 USE 5C_Convention;
 
-/*Esempio Query per Convention
+/*
+Esempio Query per Convention
 Select Piano.*
 From Piano
 Inner join Sala On Piano.Numero=Piano.Numero
 Inner join Programma On sala.nomeSala=Programma.NomeSala
-Where programma.fasciaOraria='B'*/
+Where programma.fasciaOraria='B'
+*/
 
 /*Cognome e nome dei relatori di Beintoo*/
 SELECT Relatore.cognomeRel, Relatore.nomeRel
@@ -28,3 +30,29 @@ WHERE Speech.idSpeech = `SP01`
 AND Speech.idSPeech = Programma.idSpeech
 AND Programma.nomeSala = Sala.nomeSala
 AND Sala.nPiano = Piano.nPiano;
+
+/*Conteggio dei partecipanti per ogni speech*/
+SELECT Composto.nPartecipanti,Speech.idSpeech
+FROM Programma, Speech, Composto
+WHERE Composto.idProgramma = Programma.idProgramma
+AND Speech.idSpeech = Programma.idProgramma
+GROUP BY Speech.idSpeech;
+
+/*
+SELECT avg(nPartecipanti) AS MediaPartecipanti
+FROM (SELECT COUNT(Composto.nPartecipanti) as nPartecipanti,Speech.idSpeech
+FROM Programma, Speech, Composto
+WHERE Composto.idProgramma = Programma.idProgramma
+AND Speech.idSpeech = Programma.idProgramma
+GROUP BY Speech.idSpeech) AS ConteggioPartecipanti;
+*/
+
+/*Elenco dei partecipanti dello 'Speech1' nella fascia oraria 'A' nella 'sala1' (flag arrivo nella ...)*/
+SELECT Partecipante.cognomePart, Partecipante.nomePart
+FROM Programma, Partecipante, Composto
+WHERE Partecipante.idPart = Composto.idPart
+AND Composto.idProgramma = Composto.idProgramma
+AND Programma.fasciaOraria = 'A'
+AND Programma.idSpeech = 'Speech1'
+AND Programma.nomeSala = 'Sala1'
+ORDER BY Partecipante.cognomePart ASC;
