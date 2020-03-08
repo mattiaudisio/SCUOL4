4aa1d5cf35a178b771f5ceede6362fafcb992e02
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2020-03-08 15:56:28 +0100

    modificato DDL [8/03/20]
    modificato DDL [8/03/20]

--- SQL/convention_DDL.sql
+++ SQL/convention_DDL.sql
@@ -1,87 +1,87 @@
 CREATE DATABASE 5C_Convention;
 USE 5C_Convention;
 
--- CREAZIONE TABELLA RELATORE --
-CREATE TABLE IF NOT EXISTS Relatore(
-    idRel VARCHAR(50) NOT NULL,
-    cognomeRel VARCHAR(50) NULL,
-    nomeRel VARCHAR(50) NULL,
-    idAzienda VARCHAR(50) NULL,
-    PRIMARY KEY(idRel)
-);
-
 -- CREAZIONE TABELLA AZIENDA --
-CREATE TABLE IF NOT EXISTS  Azienda(
-    idAzienda VARCHAR(50) NULL,
-    indirizzoAzienda VARCHAR(50) NULL,
-    telefonoAzienda VARCHAR(20) NULL,
+CREATE TABLE IF NOT EXISTS Azienda(
+    idAzienda VARCHAR(10) NOT NULL,
+    indirizzoAzienda VARCHAR(30) NULL,
+    telefonoAzienda VARCHAR(15) NULL,
     PRIMARY KEY(idAzienda)
 );
 
--- CREAZIONE TABELLA PROGRAMMA --
-CREATE TABLE IF NOT EXISTS  Programma(
-    idProgramma VARCHAR(50) NOT NULL,
-    fasciaOraria VARCHAR(10) NOT NULL,
-    idSpeech VARCHAR(50) NOT NULL,
-    idSala VARCHAR(50) NULL,
-    PRIMARY KEY(idProgramma)
-);
-
--- CREAZIONE TABELLA SPEECH --
-CREATE TABLE IF NOT EXISTS  Speech(
-    idSpeech VARCHAR(50) NOT NULL,
-    titolo VARCHAR(100) NULL,
-    argomento VARCHAR(200) NULL,
-    PRIMARY KEY(idSpeech)
-);
-
--- CREAZIONE TABELLA PARTECIPANTE --
-CREATE TABLE IF NOT EXISTS  Partecipante(
-    idPart VARCHAR(50) NULL,
-    cognomePart VARCHAR(50) NULL,
-    nomePart VARCHAR(50) NULL,
-    mailPart VARCHAR(50) NULL,
-    tipologiaPart VARCHAR(50) NULL,
-    PRIMARY KEY(idPart)
+-- CREAZIONE TABELLA PIANO --
+CREATE TABLE IF NOT EXISTS  Piano(
+    idPiano VARCHAR(10) NOT NULL,
+    nPiano INT NULL,
+    nSale INT NULL,
+    PRIMARY KEY(idPiano)
 );
 
 -- CREAZIONE TABELLA SALA --
 CREATE TABLE IF NOT EXISTS  Sala(
     idSala VARCHAR(50) NULL,
     nPostiSala INT(200) NULL,
-    nPiano INT(50) NULL,
+    idPiano VARCHAR(10) NULL,
     PRIMARY KEY(idSala)
 );
 
--- CREAZIONE TABELLA PIANO --
-CREATE TABLE IF NOT EXISTS  Piano(
-    nPiano INT(50) NULL,
-    nSale INT(20) NULL,
-    descrizione INT(100) NULL,
-    PRIMARY KEY(nPiano)
+-- CREAZIONE TABELLA PARTECIPANTE --
+CREATE TABLE IF NOT EXISTS  Partecipante(
+    idPart VARCHAR(10) NOT NULL,
+    cognomePart VARCHAR(20) NULL,
+    nomePart VARCHAR(20) NULL,
+    mailPart VARCHAR(30) NULL,
+    tipologiaPart VARCHAR(30) NULL,
+    PRIMARY KEY(idPart)
 );
 
--- CREAZIONE TABELLA RELAZIONA (collegamento tra RELATORE E PROGRAMMA)--
+-- CREAZIONE TABELLA RELATORE --
+CREATE TABLE IF NOT EXISTS Relatore(
+    idRel VARCHAR(10) NOT NULL,
+    cognomeRel VARCHAR(30) NULL,
+    nomeRel VARCHAR(30) NULL,
+    idAzienda VARCHAR(10) NULL,
+    PRIMARY KEY(idRel)
+);
+
+-- CREAZIONE TABELLA SPEECH --
+CREATE TABLE IF NOT EXISTS  Speech(
+    idSpeech VARCHAR(10) NOT NULL,
+    titolo VARCHAR(30) NULL,
+    argomento VARCHAR(30) NULL,
+    Durata INT NULL,
+    PRIMARY KEY(idSpeech)
+);
+
+-- CREAZIONE TABELLA PROGRAMMA --
+CREATE TABLE IF NOT EXISTS  Programma(
+    idProgramma VARCHAR(10) NOT NULL,
+    fasciaOraria VARCHAR(10) NOT NULL,
+    idSpeech VARCHAR(10) NOT NULL,
+    idSala VARCHAR(10) NOT NULL,
+    PRIMARY KEY(idProgramma)
+);
+
+-- CREAZIONE TABELLA RELAZIONE (COLLEGAMENTO TRA RELATORE E PROGRAMMA)--
 CREATE TABLE IF NOT EXISTS Relaziona(
-    idRel VARCHAR(50) NOT NULL,
-    idProgramma VARCHAR(50) NOT NULL,
+    idRel VARCHAR(10) NOT NULL,
+    idProgramma VARCHAR(10) NOT NULL,
     PRIMARY KEY(idRel,idProgramma)
 );
 
--- CREAZIONE TABELLA COMPOSTO (colegamento tra PROGRAMMA e PARTECIPANTE)--
+-- CREAZIONE TABELLA COMPOSTO (COLLEGAMENTO TRA PARTECIPANTE E PROGRAMMA)--
 CREATE TABLE IF NOT EXISTS  Composto(
-    idPart VARCHAR(50) NULL,
-    idProgramma VARCHAR(50) NOT NULL,
-    nPartecipanti INT(200) NULL,
+    idPart VARCHAR(10) NOT NULL,
+    idProgramma VARCHAR(10) NOT NULL,
+    nPartecipanti INT NULL,
     PRIMARY KEY(idPart,idProgramma)
 );
 
--- SETTO LE CHIAVI ESTERNE --
-ALTER TABLE Relatore ADD FOREIGN KEY(idAzienda) REFERENCES Azienda(idAzienda);
-ALTER TABLE Programma ADD FOREIGN KEY(idSpeech) REFERENCES Speech(idSpeech);
-ALTER TABLE Programma ADD FOREIGN KEY(idSala) REFERENCES Sala(idSala);
-ALTER TABLE Sala ADD FOREIGN KEY(nPiano) REFERENCES Piano(nPiano);
-ALTER TABLE Relaziona ADD FOREIGN KEY(idRel) REFERENCES Relatore(idRel);
-ALTER TABLE Relaziona ADD FOREIGN KEY(idProgramma) REFERENCES Programma(idProgramma);
-ALTER TABLE Composto ADD FOREIGN KEY(idPart) REFERENCES Partecipante(idPart);
-ALTER TABLE Composto ADD FOREIGN KEY(idProgramma) REFERENCES Programma(idProgramma);
+ALTER TABLE Sala ADD FOREIGN KEY (idPiano) REFERENCES Piano(idPiano);
+ALTER TABLE Relatore ADD FOREIGN KEY (idAzienda) REFERENCES Azienda(idAzienda);
+ALTER TABLE Programma  ADD FOREIGN KEY (idSpeech) REFERENCES Speech(idSPeech);
+ALTER TABLE Programma  ADD FOREIGN KEY (idSala) REFERENCES Sala(idSala);
+ALTER TABLE Relaziona ADD FOREIGN KEY (idRel) REFERENCES Relatore(idRel);
+ALTER TABLE Relaziona ADD FOREIGN KEY (idProgramma) REFERENCES Programma(idProgramma);
+ALTER TABLE Composto ADD FOREIGN KEY (idPart) REFERENCES Partecipante(idPart);
+ALTER TABLE Composto ADD FOREIGN KEY (idProgramma) REFERENCES Programma(idProgramma);
