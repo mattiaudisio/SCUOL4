a07d866a137ecfa3989846f44dc64801740ddf70
mattiaudisio <mattia.audisio.01@gmail.com>
2020-03-26 21:31:06 +0100

    modifica sito

--- Sito/index.php
+++ Sito/index.php
@@ -1,7 +1,12 @@
 <html>
 
     <head>
-    
+        <?php
+            $connessione = new msqli('localhost','root','','5C_Convention');
+            if($connessione->connection_error){
+                die('Errore di connessione ('.$connessione->connect_errno.')'. $connessione->connect_error);
+            }
+        ?>
     </head>
 
     <body>
