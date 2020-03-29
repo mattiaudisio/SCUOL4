e1aa65bd40e7ed5bed98ea0fd273201c67c8a2d9
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2020-03-29 17:48:40 +0200

    modifica index.php

--- Sito/index.php
+++ Sito/index.php
@@ -1,20 +1,25 @@
-<html>
+<?php
+    $connessione = new msqli('localhost','root','','5C_Convention');
+    if($connessione->connection_error){
+        die('Errore di connessione ('.$connessione->connect_errno.')'. $connessione->connect_error);
+    }
+    $connessione -> query("SET NAMES 'utf8'");
+?>
 
+<html>
     <head>
+        <title>Convention Audisio</title>  
+    </head>
+    <body>
+        <div align="center">Convention Audisio</div>
         <?php
-            $connessione = new msqli('localhost','root','','5C_Convention');
-            if($connessione->connection_error){
-                die('Errore di connessione ('.$connessione->connect_errno.')'. $connessione->connect_error);
+            $risultato = $connessione->("SELECT * FROM Azienda");
+            while($tabella = $risultato->fetch_assoc()){
+                echo $tabella['idAzienda'];
+                echo "<br />";
             }
         ?>
-    </head>
-
-    <body>
-
     </body>
-
     <footer>
-
     </footer>
-
 </html>
