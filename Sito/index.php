67924d6077aa6fd663a7e9d66483531fb1f483c6
mattiaudisio <mattia.audisio.01@gmail.com>
2020-04-02 17:01:24 +0200

    Aggiunte pagine iscriviti e riepilogo

--- Sito/index.php
+++ Sito/index.php
@@ -1,7 +1,9 @@
 <?php
-    $connessione = new msqli('localhost','root','','5C_Convention');
+    $connessione = new mysqli('localhost','root','','5C_Convention');
     if($connessione->connection_error){
         die('Errore di connessione ('.$connessione->connect_errno.')'. $connessione->connect_error);
+    }else{
+        echo 'Connesso. ' . $connessione->host_info . "\n";
     }
     $connessione -> query("SET NAMES 'utf8'");
 ?>
@@ -11,14 +13,13 @@
         <title>Convention Audisio</title>  
     </head>
     <body>
-        <div align="center">Convention Audisio</div>
-        <?php
-            $risultato = $connessione->("SELECT * FROM Azienda");
-            while($tabella = $risultato->fetch_assoc()){
-                echo $tabella['idAzienda'];
-                echo "<br />";
-            }
-        ?>
+        <div align="center">
+            <h1> <b>Convention Audisio</b> </h1>
+        </div>
+        <div align="center">
+            <h3> <a href="../Sito/paginaIscrizione.html">Iscrizione</a> <br> </h3>
+            <h3> <a href="../Sito/paginaRiepilogo.php">Riepilogo</a> </h3>
+        </div>
     </body>
     <footer>
     </footer>
