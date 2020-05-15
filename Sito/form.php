0135c20f920ce19b58d77ab0f56fe95caf672598
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-15 17:54:57 +0200

    modifiche form.php

--- Sito/form.php
+++ Sito/form.php
@@ -149,9 +149,10 @@
                                   $risIdProgr = $ris["idProgramma"];
                                   $risFascia = $ris["fasciaOraria"];
                                   $risTitolo = $ris["titolo"];
-                                  $risPosti = $ris["nPostiSala"];
+                                  $risPosti = $ris["numPosti"];
                                   $risIdSala = $ris["idSala"];
-                                  $nuovoOggetto = new Programma($risFascia,$risTitolo,$risPosti,$risIdProgr,$risIdSala);
+                                  $risImmagine = $ris["immagine"];
+                                  $nuovoOggetto = new Programma($risFascia,$risTitolo,$risPosti,$risIdProgr,$risIdSala,$risImmagine);
                                   array_push($arrayProgramma,$nuovoOggetto);
                                 }
                               for($j = 0; $j < sizeof($checkbox); $j++){
