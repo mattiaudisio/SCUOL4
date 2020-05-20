c707c6c0f65dd9c8c4824625424d6903026ec2d0
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-20 13:06:22 +0200

    modifiche autenticazione sessione

--- Sito/Funzioni_PHP/header_autenticazione.php
+++ Sito/Funzioni_PHP/header_autenticazione.php
@@ -1,13 +1,36 @@
 <?php
-  session_start();
-  if(!isset($_POST['mail']) || !isset($_POST['password'])){
-    header('location: /Mattia/ProgettoSQL_Convention/Sito/login_failed.php');
-    exit;
-  }else{
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
+  include_once(__DIR__.'/connesione.php');
+  $connesione = Connesione::apriConnessione();
 
-    $_SESSION['mail'] = $mail;
-    $_SESSION['password'] = $password;
+  session_start();
+
+  $errore = "";
+
+  if(isset($_POST['accedi'])){
+    if(empty($_POST['mail']) || empty($_POST['password']) ){
+      $errore = "Mail o password non valida";
+      header("location: login.php");
+    }else{
+      $mail = $_POST['mail'];
+      $password = $_POST['password'];
+
+      $queryLogin = $connesione->query("SELECT Partecipante.mailPart, Partecipante.passwordPart FROM Partecipante WHERE mailPart='".$mail."'");
+      if($row = $queryLogin->fetch_row()){
+        if(password_verify($password,$row[1])){
+          $_SESSION['mail_user'] = $username
+          header("location: profilo.php");
+        }else{
+          $errore = "Mail o password non corrette"
+          header("location: login.php");
+        }
+      }else{
+        $errore = "Mail o password non corrette"
+        header("location: login.php");
+      }
+    }
+    $connessione->close();
+  }else{
+    $errore = "Mail o password non corrette"
+    header("location: login.php");
   }
 ?>
