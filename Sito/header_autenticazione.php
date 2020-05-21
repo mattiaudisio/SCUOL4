d98979ff5563dafd0260fc0d1adefe6713ec5167
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-21 19:26:04 +0200

    modifiche

--- Sito/header_autenticazione.php
+++ Sito/header_autenticazione.php
@@ -8,17 +8,17 @@
   if (isset($_POST['submit'])) {
     if (empty($_POST['mail']) || empty($_POST['password'])) {
       $error = "Mail o password non valida";
-      header("location: index.php");
+      header("location: login.php");
     }else{
       $mail = $_POST['mail'];
       $password = hash('sha256',$_POST['password']);
 
-      $queryLogin = $connessione->query("SELECT Partecipante.mailPart, Partecipante.passwordPart FROM Partecipante WHERE Partecipante.mailPart='".$mail."';");
+      $queryLogin = $connessione->query("SELECT Partecipante.passwordPart FROM Partecipante WHERE Partecipante.mailPart='".$mail."';");
 
-      if($row = $queryLogin->fetch_assoc()){ //controllo se la mail esiste nel db
-        if(password_verify($password, $row['passwordPart'])){ 
+      if($row = $queryLogin->fetch_assoc()){
+        if(password_verify($password, $row)){
           $_SESSION['mail_user'] = $mail;
-          header("location: utente/profilo.php");
+          header("location: ../Sito/profilo.php");
         }else {
           $errore = "Mail o password non corrette";
           header("location: ../Sito/index.php");
