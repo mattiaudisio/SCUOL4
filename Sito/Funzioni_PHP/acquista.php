85eaac7e4d4f27f40fd119a9204e2ea438aec477
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-21 16:19:36 +0200

    modifiche acquista.php

--- Sito/Funzioni_PHP/acquista.php
+++ Sito/Funzioni_PHP/acquista.php
@@ -14,7 +14,7 @@
         }else{
           $mail = $_POST['mail'];
           $password = $_POST['password'];
-          $queryLogin = $connessione->query("SELECT Partecipante.mailPart, Partecipante.passwordPart, Partecipante.idPart FROM Partecipante WHERE mailPart='".$mail."'");
+          $queryLogin = $connessione->query("SELECT Partecipante.mailPart, Partecipante.passwordPart, Partecipante.idPart, Speech.titolo FROM Partecipante, Speech WHERE mailPart='".$mail."'");
           while($row = $queryLogin->fetch_assoc()){
               if(password_verify($password,$row[1])){
                   $passwordCifrata = hash('sha256',$_POST['password']);
@@ -23,6 +23,12 @@
                     for($i = 0; $i < sizeof($checkbox); $i++){
                       $query = "INSERT INTO Composto(idPart, idProgramma, nPartecipanti) VALUES  ('".$row[2]."','".$checkbox[$i]."','".$persone."');";
                       $connessione->query($query);
+                      $quey2 = "SELECT Speech.numPosti FROM Speech"
+                      while($var = $quey2->fetch_assoc()){
+                        $posti = $var[0] - 1;
+                        $query3 = "UPDATE Speech SET numPosti = ".$posti."  WHERE titolo = '".$row[3]."'";
+                        $connessione->query($query3);
+                      }
                     }
                   }
                   $queryEliminazionePosti = $connessione->query("SELECT ");
