97b3b773975aa1f3ed80ea40a132cee1b939d65d
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-27 19:11:25 +0200

    modifiche

--- Sito/acquista.php
+++ Sito/acquista.php
@@ -6,23 +6,32 @@
 
       $errore = "";
 
-          $mail = $_SESSION['mail_user'];
-          $queryLogin = $connessione->query("SELECT Partecipante.passwordPart, Partecipante.idPart, Speech.titolo FROM Partecipante, Speech WHERE mailPart='".$mail."'");
-          while($row = $queryLogin->fetch_assoc()){
-                  $checkbox = $_POST['interessi'];
-                  if($connessione->query($dml1) === TRUE){
-                    for($i = 0; $i < sizeof($checkbox); $i++){
-                      $query = "INSERT INTO Composto(idPart, idProgramma, nPartecipanti) VALUES  ('".$row[1]."','".$checkbox[$i]."','".$persone."');";
-                      $connessione->query($query);
-                      $quey2 = "SELECT Speech.numPosti FROM Speech";
-                      while($var = $quey2->fetch_assoc()){
-                        $posti = $var[0] - 1;
-                        $query3 = "UPDATE Speech SET numPosti = ".$posti."  WHERE titolo = '".$row[3]."'";
-                        $connessione->query($query3);
-                      }
-                    }
-                  }
-                  $queryEliminazionePosti = $connessione->query("SELECT ");
-                  header("location: ../Sito/profilo.php");
+      if(isset($_POST['acquista'])){
+        $mail = $_SESSION['mail_user'];
+        $checkbox = $_POST['interessi'];
+
+        $query0 = "SELECT Partecipante.passwordPart, Partecipante.idPart FROM Partecipante WHERE mailPart='".$mail."';";
+        $queryLogin = $connessione->query($query0);
+        if($row = $queryLogin->fetch_assoc()){
+          for($i = 0; $i < sizeof($checkbox); $i++){
+            $queryNum = "SELECT Composto.nPartecipanti FROM Composto WHERE idPart='".$row["idPart"]."';";
+            $queryNumero = $connessione->query($queryNum);
+            if($var = $queryNumero->fetch_assoc()){
+              $persone = $var['nPartecipanti'] + 1;
+            }
+            $query = "INSERT INTO Composto(idPart, idProgramma, nPartecipanti) VALUES  ('".$row["idPart"]."','".$checkbox[$i]."','".$persone."');";
+            if($connessione->query($query) === TRUE){
+              for($i = 0; $i < sizeof($checkbox); $i++){
+                $query2 = "SELECT Speech.numPosti FROM Speech";
+                while($var = $query2->fetch_assoc()){
+                  $posti = $var[0] - 1;
+                  $query3 = "UPDATE Speech SET numPosti = ".$posti."  WHERE titolo = '".$row[3]."'";
+                  $connessione->query($query3);
+                }
+              }
+              header("location: ../Sito/profilo.php");
+            }
+          }
         }
+      }
 ?>
