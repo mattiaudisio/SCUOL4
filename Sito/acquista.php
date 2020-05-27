d0cf01d4e338955e41e97f10656d885eef671d5e
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-27 16:56:54 +0200

    modifiche

--- Sito/acquista.php
+++ Sito/acquista.php
@@ -1,23 +1,18 @@
 <?php
-  class Acquista{
-
-    static function aquistaSpeech(){
-      include_once(__DIR__.'/connesione.php');
+      include_once(__DIR__.'/Funzioni_PHP/connessione.php');
       $connessione = Connessione::apriConnessione();
+
+      session_start();
 
       $errore = "";
 
-      if(isset($_POST['acqusita'])){
-          $mail = $_POST['mail'];
-          $password = $_POST['password'];
-          $queryLogin = $connessione->query("SELECT Partecipante.mailPart, Partecipante.passwordPart, Partecipante.idPart, Speech.titolo FROM Partecipante, Speech WHERE mailPart='".$mail."'");
+          $mail = $_SESSION['mail_user'];
+          $queryLogin = $connessione->query("SELECT Partecipante.passwordPart, Partecipante.idPart, Speech.titolo FROM Partecipante, Speech WHERE mailPart='".$mail."'");
           while($row = $queryLogin->fetch_assoc()){
-              if(password_verify($password,$row[1])){
-                  $passwordCifrata = hash('sha256',$_POST['password']);
                   $checkbox = $_POST['interessi'];
                   if($connessione->query($dml1) === TRUE){
                     for($i = 0; $i < sizeof($checkbox); $i++){
-                      $query = "INSERT INTO Composto(idPart, idProgramma, nPartecipanti) VALUES  ('".$row[2]."','".$checkbox[$i]."','".$persone."');";
+                      $query = "INSERT INTO Composto(idPart, idProgramma, nPartecipanti) VALUES  ('".$row[1]."','".$checkbox[$i]."','".$persone."');";
                       $connessione->query($query);
                       $quey2 = "SELECT Speech.numPosti FROM Speech";
                       while($var = $quey2->fetch_assoc()){
@@ -29,15 +24,5 @@
                   }
                   $queryEliminazionePosti = $connessione->query("SELECT ");
                   header("location: ../Sito/profilo.php");
-              }else{
-                $errore = "Errore durante l'acquisto";
-                header("location: ../Sito/index.php#speaker.php");
-              }
-          }
-      }else{
-        $errore = "Errore durante l'acquisto";
-        header("location: ../Sito/index.php#programma.php.php");
-      }
-    }
-  }
+        }
 ?>
