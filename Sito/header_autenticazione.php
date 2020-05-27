1943e0de9ea1c70fd161ead36cf59a2a5dac6a47
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-27 18:01:49 +0200

    modifiche

--- Sito/header_autenticazione.php
+++ Sito/header_autenticazione.php
@@ -10,22 +10,18 @@
       $error = "Mail o password non valida";
       header("location: login.php");
     }else{
-      $mail = $_POST['mail'];
+      $_SESSION['mail_user'] = $_POST['mail'];
       $password = hash('sha256',$_POST['password']);
-      //$password = $_POST['password'];
 
-      $queryLogin = $connessione->query("SELECT Partecipante.passwordPart FROM Partecipante WHERE Partecipante.mailPart='".$mail."';");
+      $queryLogin = $connessione->query("SELECT Partecipante.passwordPart FROM Partecipante WHERE Partecipante.mailPart='".$_SESSION["mail_user"]."';");
 
       if($row = $queryLogin->fetch_assoc()){
         if(password_verify($password, $row['passwordPart'])){
-          $_SESSION['mail_user'] = $mail;
           header("location: ../Sito/profilo.php");
         }else {
-          $errore = "Mail o password non corrette";
-          header("location: ../Sito/index.php");
+          header("location: ../Sito/login.php");
         }
       }else{
-        $errore = "Mail o password non corrette";
         header("location: ../Sito/login.php");
       }
     }
