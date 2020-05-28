0a5afb65617bc7c95bd113cb6083494144c03eba
mattiaudisio <mattia.audisio.01@gmail.com>
2020-05-28 21:40:25 +0200

    funziona

--- Sito/acquista.php
+++ Sito/acquista.php
@@ -35,31 +35,13 @@
 
       if(isset($_POST['elimina'])){
           $checkbox = $_POST['interessi'];
-          /*$queryLogin = $connessione->query("SELECT Partecipante.passwordPart, Partecipante.idPart, Programma.idProgramma FROM Partecipante, Programma, Composto WHERE Partecipante.idPart='".$_SESSION["idPart"]."' AND Partecipante.idPart = Composto.idPart AND Composto.idProgramma = Programma.idProgramma");
+          $query0 = "SELECT Partecipante.passwordPart, Partecipante.idPart FROM Partecipante WHERE Partecipante.idPart='".$_SESSION["idPart"]."';";
+          $queryLogin = $connessione->query($query0);
           if($row = $queryLogin->fetch_assoc()){
-            if($row == TRUE){
-                $idPart = $row['idPart'];
-                $idProgramma = $row['idProgramma'];
-                $querySpeech = $connessione->query("SELECT Speech.numPosti, Speech.titolo, Sala.nPostiSala FROM Speech,Sala");
-                while($var  = $querySpeech->fetch_assoc()){
-                    $posti = $var['numPosti'] + 1;
-                    $titolo = $var['titolo'];
-                    $connessione->query("UPDATE Speech SET numPosti = $posti  WHERE titolo = '$titolo'");
-                 }
-                 header("location: ../Sito/profilo.php");
+            for($i = 0; $i < sizeof($checkbox); $i++){
+              $queryEliminazione = $connessione->query("DELETE FROM Composto WHERE Composto.idPart = '".$row["idPart"]."' AND Composto.idProgramma = '".$checkbox[$i]."';");
+               header("location: ../Sito/profilo.php");
             }
-          }*/
-          for($i = 0; $i < sizeof($checkbox); $i++){
-            $idPart = $_SESSION['idPart'];
-            $queryEliminazione = $connessione->query("DELETE FROM Compsoto WHERE Composto.idPart = $idPart AND Composto.idProgramma = $checkbox[$i]");
-            /*while($var  = $queryEliminazione->fetch_row()){
-                if($var == TRUE){
-                  $posti = $var['numPosti'] + 1;
-                  $titolo = $var['titolo'];
-                  $connessione->query("UPDATE Speech SET numPosti = $posti  WHERE titolo = '$titolo'");
-                }
-             }*/
-             header("location: ../Sito/profilo.php");
           }
         }
 ?>
