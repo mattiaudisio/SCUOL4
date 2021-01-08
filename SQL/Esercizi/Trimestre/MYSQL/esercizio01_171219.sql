48de20fb8c5d240af8cf1cc4e96b55074a907ef9
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2021-01-08 23:16:28 +0100

    Delete esercizio01_171219.sql

--- SQL/Esercizi/Trimestre/MYSQL/esercizio01_171219.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE esercizioProva (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Prova_1` VARCHAR(45) NULL,
-  `Prova_2` VARCHAR(45) NULL);
-
-ALTER TABLE esercizioProva ADD PRIMARY KEY (`id`);
