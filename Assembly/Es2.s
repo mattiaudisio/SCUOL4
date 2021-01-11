f417612b8f8ddeaa56f526ad27e502a41da084de
Mattia Audisio <mattia.audisio.01@gmail.com>
2021-01-11 19:51:34 +0100

    Update Es2.s

--- Assembly/Es2.s
+++ Assembly/Es2.s
@@ -1,60 +1,36 @@
-// Chiedi in input 2 stringhe[ str 1(input 1), str 2(input 2)]
-// creare una terza strings che contiene le altre 2  
+//Mattia Audisio
 
 .data
-    
-    str1:       .space 30
-    str2:       .space 30
-    str3:       .space 60
-                                                                     
-    str1_len:    .long 0
-    str2_len:    .long 0
-    str3_len:    .long 0
+    vet:    .long   14,27,32,55,89,2,17,29,48,1
+    v_ln    .-vet
 
 .text
     .global main
-
 main:
-    //inserisco la prima stringa
+    //leggo la stringa
     movl    $3,%eax
     movl    $0,%ebx
-    movl    $str1,%ecx
-    movl    $30,%edx
-    int     $0x80
+    movl    $vet,%ecx
+    movl    $v_ln,%edx
+    int     0x80
     
-    //mi salvo la dimensione della prima stringa
-    decl    %eax                #escludo dalla prima stringa il \n
-    movl    %eax,str1_len
+    movl    $v_ln,%edi
+loop:
+    //metto $vet[%edi] in ECX
+    movl    $vet,%ecx
+    movl    %edi,%ecx
 
-    movl    str1,%eax
-    movl    %eax, str3          #salvo la prima stringa in str3
+    //stampo $vet[%edi]
+    movl    $4,%eax
+    movl    $1,%ebx
+    movl    $1,%edx
+    int     $0x80
 
-    //inserisco la seconda stringa
-    movl    $3,%eax
+    //decremento EDI finche non arrivo a 0, poi esco dal ciclo
+    decl    %edi
+    cmpl    $-1,%edi
+    jne     loop
+
+    movl    $1,%eax
     movl    $0,%ebx
-    movl    $str2,%ecx
-    movl    $30,%edx
     int     $0x80
-
-    //mi salvo la dimensione della seconda stringa senza decremento [ così mantengo il \n ]
-    movl %eax, str2_len
-    
-    //accodo la seconda stringa a str3
-    movl    str1_len, %ecx
-    movl    str2, %eax
-    movl    %eax, str3(%ecx)    #posso scrivere così perchè mi devo spostare di un solo carattere
-
-    addl    str2_len, %ecx      #sommo la dimensione di str2 a str1_len che era salvato in %ecx            
-    movl    %ecx, %edx          #in %edx tengo la dimensione della stringa
-
-    //stampo la stringa
-    movl   $4,%eax
-    movl   $1,%ebx
-    movl   $str3,%ecx
-    #non salvo nulla in %edx perchè ho già salvato la dimensione della stringa in precedenza 
-    int    $0x80
-
-    movl   $1,%eax 
-    movl   $0,%ebx
-    int    $0x80
-
