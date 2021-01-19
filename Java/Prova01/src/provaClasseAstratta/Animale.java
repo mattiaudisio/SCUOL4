8340b5433d949c0ee1ec9e6dcba387c25d513e34
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2021-01-19 22:46:16 +0100

    Update Animale.java

--- Java/Prova01/src/provaClasseAstratta/Animale.java
+++ Java/Prova01/src/provaClasseAstratta/Animale.java
@@ -4,12 +4,13 @@ public interface Animale {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		mangia();	//Posso avere un interfaccia con un attributo non astratto; questo non è dichiarato come metodo astratto; definisce un metodo pubblico normalissimo
+		mangia();	//Posso avere un interfaccia con un attributo non astratto; questo non Ã¨ dichiarato come metodo astratto; definisce un metodo pubblico normalissimo
 		//public void dorme();	//I METODI ASTRATTI NON HANNO BISOGNO DI UN CORPO; QUINDI NIENTE {}
 	}
 
 	public static void mangia() {
 		// TODO Auto-generated method stub
+		System.out.println("\nSto mangiando");
 		
 	}
 
