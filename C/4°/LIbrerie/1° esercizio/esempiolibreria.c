038bbee7bdd57a326a647db798f679c412d27d2d
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2021-01-21 10:59:24 +0100

    Update esempiolibreria.c

--- C/4°/LIbrerie/1° esercizio/esempiolibreria.c
+++ C/4°/LIbrerie/1° esercizio/esempiolibreria.c
@@ -19,3 +19,11 @@ int main(){
 	printf("Il totale e' %d\n",tot);
 return 0;
 }
+
+int somma(int *vet1,int lunghezza){
+	int i,som=0;
+	for(i=0;i<lunghezza;i++){
+		som=som+vet1[i];
+	}
+return som;
+}
