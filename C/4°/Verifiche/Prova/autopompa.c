b8ffd7939a40021b6749ecfa19779708e53652ac
Mattia Audisio <57859960+mattiaudisio@users.noreply.github.com>
2021-01-21 11:14:29 +0100

    Update autopompa.c

--- C/4°/Verifiche/Prova/autopompa.c
+++ C/4°/Verifiche/Prova/autopompa.c
@@ -1,57 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <sys/sem.h>
-#include <unistd.h>
 
 struct sembuf buffer;
 
-void sem_lock(int semid);
-void sem_unlock(int semid);
+void sem_lock(int);
+void sem_unlock(int);
 
 int main(){
-	pid_t pid,pid_fork;
-	key_t sem_chiave=7742,mem_chiave=1156;
-	int semstato,M,N,*litri,i,flag=0,num,shmid,semid,stato;
+	key_t memchiave,semchiave;
+	int i,M,N,*pompa,shmid,semid,num,semstato;
 
-	shmid=shmget(mem_chiave,1024, IPC_EXCL | 0666);
-	semid=semget(sem_chiave,1, IPC_EXCL | 0666);
-	if(shmid<=-1 || semid<=-1){
-		printf("\nErrore nella connessione");
+	memchiave=7881;
+	semchiave=6534;
+	shmid=shmget(memchiave,1024,IPC_EXCL|0666);
+	semid=semget(semchiave,1,IPC_EXCL|0666);
+	if(shmid==-1 || semid==-1){
+		printf("\nErrore nella connessione\n");
+		exit(-1);
 	}
 
-	litri=(int *)shmat(shmid,NULL,0);
-	litri[1]=0;
+	pompa=(int *)shmat(shmid,NULL,0);
+	pompa[1]=0;
 
-	while(litri[1]!=1){
-		if(litri[0]<=15){
+	while(pompa[1]!=1){
+		if(pompa[0]<=15){
 			sem_lock(semid);
-			printf("\nSto facendo rifornimento\n");
-			litri[0]=litri[2];
-			sleep(5);			
+			printf("\nSto facendo riforniemnto\n");
+			pompa[0]=pompa[2];
+			sleep(5);
 			sem_unlock(semid);
 		}
 	}	
-	printf("\nHo finito\n");
+	printf("\nHo finito\n");	
 return 0;
 }
 
-void sem_lock(int semid){
-	buffer.sem_num=0;
-	buffer.sem_flg=0;
-	buffer.sem_op=-1;
-	if(semop(semid, &buffer, 1)==-1)
-		printf("\nErrore blocco semaforo\n");
+void sem_lock (int semid){
+    buffer.sem_num=0; 
+    buffer.sem_flg=0;
+    buffer.sem_op=-1;
+    if(semop(semid, &buffer, 1)==-1){
+        printf("Errore blocco semaforo\n");
+	}
 }
-
-void sem_unlock(int semid){
-	buffer.sem_num=0;
-	buffer.sem_flg=0;
-	buffer.sem_op=1;
-	if(semop(semid, &buffer, 1)==-1)
-		printf("\nErrore sblocco semaforo\n");
+void sem_unlock (int semid){
+    buffer.sem_num=0;
+    buffer.sem_flg=0;
+    buffer.sem_op=1;
+    if(semop(semid, &buffer, 1)==-1){
+        printf("Errore sblocco semaforo\n");
+	}
 }
