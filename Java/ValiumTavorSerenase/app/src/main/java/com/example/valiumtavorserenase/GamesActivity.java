11256cec6c8608fd9c21ee8191789b5aa639c713
Mattia Audisio <mattia.audisio.01@gmail.com>
2021-05-16 20:54:17 +0200

    Migliorie HomeActivity

---
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/GamesActivity.java
@@ -0,0 +1,22 @@
+package com.example.valiumtavorserenase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+
+
+    }
+}
--- Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/HomeActivity.java
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.valiumtavorserenase;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,11 +12,10 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    RecyclerView dataList;
-    List<String> titles;
-    List<Integer> images;
-    Adapter adapter;
-
+    private RecyclerView dataList;
+    private List<String> titles;
+    private List<Integer> images;
+    private AdapterHome adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -27,28 +28,29 @@ public class HomeActivity extends AppCompatActivity {
         images = new ArrayList<>();
 
         titles.add("Games");
-        titles.add("Second Item");
-        titles.add("Third Item");
-        titles.add("Fourth Item");
-        titles.add("First Item");
-        titles.add("Second Item");
-        titles.add("Third Item");
-        titles.add("Fourth Item");
+        titles.add("App");
+        titles.add("Impostazioni");
 
         images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
-        images.add(R.drawable.games);
+        images.add(R.drawable.apppredefinite);
+        images.add(R.drawable.impostazioni);
 
-        adapter = new Adapter(this,titles,images);
+        adapter = new AdapterHome(this,titles,images);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
         dataList.setLayoutManager(gridLayoutManager);
         dataList.setAdapter(adapter);
+    }
+
+    public void createIntent(){
+        Toast.makeText(this.getApplicationContext(),"Ciao",Toast.LENGTH_SHORT).show();
+        /*switch (adapterPosition){
+            case 0:
+                Intent gamesIntent = new Intent(HomeActivity.this, GamesActivity.class);
+                startActivity(gamesIntent);
+                break;
+
+        }*/
     }
 
 }
