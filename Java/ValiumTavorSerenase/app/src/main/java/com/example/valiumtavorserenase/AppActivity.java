b645b8cbb2bd06995f3fae0a6019a6b48ced79d7
Mattia Audisio <mattia.audisio.01@gmail.com>
2021-05-17 22:18:59 +0200

    Update

--- Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/AppActivity.java
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/AppActivity.java
@@ -1,14 +1,39 @@
 package com.example.valiumtavorserenase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppActivity extends AppCompatActivity {
+
+    private RecyclerView dataList;
+    private List<String> titles;
+    private List<Integer> images;
+    private AdapterApp adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        dataList = findViewById(R.id.dataListGames);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        titles.add("Progetto Intent");
+
+        images.add(R.drawable.progettointent);
+
+        adapter = new AdapterApp(this,titles,images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
     }
 }
--- Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/ImpostazioniActivity.java
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/ImpostazioniActivity.java
@@ -1,10 +1,18 @@
 package com.example.valiumtavorserenase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class ImpostazioniActivity extends AppCompatActivity {
+
+    private RecyclerView dataList;
+    private List<String> titles;
+    private List<Integer> images;
+    private AdapterGames adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
