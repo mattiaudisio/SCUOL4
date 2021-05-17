c8f7f1f370cacc6c3702f342177c97f6e14081be
Mattia Audisio <mattia.audisio.01@gmail.com>
2021-05-17 21:22:25 +0200

    Animazioni quando entri in sezioni

---
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/AdapterGames.java
@@ -0,0 +1,78 @@
+package com.example.valiumtavorserenase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdapterGames extends RecyclerView.Adapter<AdapterGames.ViewHolder>{
+    Context context;
+    List<String> titles;
+    List<Integer> images;
+    LayoutInflater inflater;
+
+    public AdapterGames(Context ctx, List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(ctx);
+        this.context = ctx;
+    }
+
+    @NonNull
+    @Override
+    public AdapterGames.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.custom_grid_layout,parent,false);
+        return new AdapterGames.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterGames.ViewHolder holder, int position) {
+        holder.title.setText(titles.get(position));
+        holder.gridIcon.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView gridIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textView2);
+            gridIcon = itemView.findViewById(R.id.imageView2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent;
+                    switch (getAdapterPosition()){
+                        case 0:
+                            intent = new Intent(context , TicTacToeActivity.class);
+                            context.startActivity(intent);
+                            break;
+                        case 1:
+                            intent = new Intent(context , AppActivity.class);
+                            context.startActivity(intent);
+                            break;
+                        case 2:
+                            intent = new Intent(context , ImpostazioniActivity.class);
+                            context.startActivity(intent);
+                            break;
+                    }
+                }
+            });
+        }
+    }
+}
--- Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/GamesActivity.java
+++ Java/ValiumTavorSerenase/app/src/main/java/com/example/valiumtavorserenase/GamesActivity.java
@@ -12,11 +12,30 @@ import java.util.List;
 
 public class GamesActivity extends AppCompatActivity {
 
+    private RecyclerView dataList;
+    private List<String> titles;
+    private List<Integer> images;
+    private AdapterGames adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
 
+        dataList = findViewById(R.id.dataListGames);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        titles.add("Tic Tac Toe");
+
+        images.add(R.drawable.tictactoe);
+
+        adapter = new AdapterGames(this,titles,images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
 
     }
 }
